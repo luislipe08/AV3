8766937cf66b154c0350f4cb10e623d292f8af9c
Luis Felipe Ferreira <luislipe08@gmail.com>
2017-10-06 15:21:59 -0300

    AV3
    Seletor poções

---
+++ Assets/Editor/Lista.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(PotionSelectorAttribute))]
+
+public class Lista : PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        int index = 0;
+        string[] potions = new string[PotionDB.Instance.potions.Length];
+        for (int i = 0;  i < PotionDB.Instance.potions.Length; i++)
+        {
+            potions[i] = PotionDB.Instance.potions[i].name;
+            if (potions[i] == property.stringValue)
+            {
+                index = i;
+            }
+
+        }
+        index = EditorGUI.Popup(position, index, potions);
+        property.stringValue =  potions[index];
+    }
+
+}
---
+++ Assets/Scripts/TestBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.AttributeUsage(System.AttributeTargets.Field)]
+public class PotionSelectorAttribute : PropertyAttribute
+{
+}
+public class TestBehaviour : MonoBehaviour
+{
+	[PotionSelector]
+	public string potion;
+    public void Update()
+    {
+        for (int i = 0; i < PotionDB.Instance.potions.Length; i++ )
+        {
+            if (PotionDB.Instance.potions[i].name == potion)
+            {
+                Debug.Log(PotionDB.Instance.potions[i].heal);
+
+            }
+            
+            }
+    }
+}
+//Dudu que me ajudou
+   
+
